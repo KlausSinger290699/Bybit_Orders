6510a460172358b751f5b3075a302baa7ca240cc
Klaus Singer <klauspapiom@homtail.com>
2025-09-18 16:58:53 +0200

    Folder structure for data

---
+++ Scripts/Trading_Bot_Test3/CatchJS_Data_WS/Unused/bull_div_subscriber.py
@@ -0,0 +1,23 @@
+﻿# bull_div_subscriber.py
+import bull_div_reader_sync
+
+#TODO: Use Websockets instead of events to communicate with bull_div_reader_sync
+
+# simple counters to verify we get everything
+count = {"n": 0}
+
+@bull_div_reader_sync.bus.on("divergence")
+def handle_divergence(data):
+    count["n"] += 1
+    # Print a compact sanity line
+    print(
+        f"📩 Received #{count['n']} | "
+        f"L1={data['L1']} T1={data['T1']} | "
+        f"L2={data['L2']} T2={data['T2']} | "
+        f"L3={data['L3']} T3={data['T3']} | "
+        f"L4={data['L4']} T4={data['T4']} | "
+        f"SL={data['SL']} | Tradable={data['Tradable']}"
+    )
+
+if __name__ == "__main__":
+    bull_div_reader_sync.main()
