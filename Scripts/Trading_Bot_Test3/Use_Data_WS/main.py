2f49cf081d9bb9128e20767134510f27556e8758
Klaus Singer <klauspapiom@homtail.com>
2025-09-20 17:59:00 +0200

    update

--- Scripts/Trading_Bot_Test3/Use_Data_WS/main.py
+++ Scripts/Trading_Bot_Test3/Use_Data_WS/main.py
@@ -1,3 +1,6 @@
+# main.py
+import asyncio
 from Scripts.Trading_Bot_Test3.Use_Data_WS.CatchData import ws_receiver_bridge_8765 as recv
 
-recv.start_client("ws://127.0.0.1:8765", noisylogs=False, logs=False)
+if __name__ == "__main__":
+    asyncio.run(recv.run("ws://127.0.0.1:8765", noisylogs=True, logs=False))
